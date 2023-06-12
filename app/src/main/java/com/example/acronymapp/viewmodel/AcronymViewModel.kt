027931db7686429ad11923afd612869fee59aa75
Sriram <sriram857raja@gmail.com>
2023-06-12 11:55:07 +0530

    Scope changes and Job added.

--- app/src/main/java/com/example/acronymapp/viewmodel/AcronymViewModel.kt
+++ app/src/main/java/com/example/acronymapp/viewmodel/AcronymViewModel.kt
@@ -3,14 +3,13 @@ package com.example.acronymapp.viewmodel
 import android.content.Context
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.example.acronymapp.R
 import com.example.acronymapp.helper.RetrofitHelper
 import com.example.acronymapp.helper.RetrofitInstance
 import com.example.acronymapp.model.AcronymModel
 import kotlinx.coroutines.CoroutineExceptionHandler
-import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.Job
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
 
@@ -21,7 +20,6 @@ class AcronymViewModel : ViewModel() {
     val acronymResponse: MutableLiveData<AcronymModel> = MutableLiveData()
     val errorMessage = MutableLiveData<String>()
     val progressBar = MutableLiveData(false)
-    private var job: Job? = null
     private val exceptionHandler = CoroutineExceptionHandler { _, throwable ->
         onError("Error : ${throwable.localizedMessage}")
     }
@@ -29,7 +27,7 @@ class AcronymViewModel : ViewModel() {
     fun getAcronymData(context: Context, sf: String, lf: String) {
         progressBar.value = true
         val api = RetrofitInstance.getInstance.create(RetrofitHelper::class.java)
-        job = CoroutineScope(Dispatchers.IO + exceptionHandler).launch {
+        viewModelScope.launch(Dispatchers.IO + exceptionHandler) {
             val response = api.getAcronymInfo(sf, lf)
             withContext(Dispatchers.Main) {
                 response?.let {
@@ -39,7 +37,7 @@ class AcronymViewModel : ViewModel() {
                                 onError(context.getString(R.string.no_records))
                             } else {
                                 progressBar.postValue(false)
-                                acronymResponse.postValue(response.body()!![0])
+                                acronymResponse.postValue(it[0])
                             }
                         }
                     } else {
@@ -53,11 +51,6 @@ class AcronymViewModel : ViewModel() {
     private fun onError(message: String) {
         errorMessage.postValue(message)
         progressBar.postValue(false)
-    }
-
-    override fun onCleared() {
-        super.onCleared()
-        job?.cancel()
     }
 
 
